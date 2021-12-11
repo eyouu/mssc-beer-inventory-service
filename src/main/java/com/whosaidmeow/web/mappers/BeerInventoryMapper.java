89d5572067e77b4a9864375a80a89e70acbb78ea
Vasyl Zhovnir <vasyl.zhovnir@cgm.com>
2021-12-11 15:20:35 +0200

    init

---
+++ src/main/java/com/whosaidmeow/web/mappers/BeerInventoryMapper.java
@@ -0,0 +1,13 @@
+package com.whosaidmeow.web.mappers;
+
+import com.whosaidmeow.domain.BeerInventory;
+import com.whosaidmeow.web.model.BeerInventoryDto;
+import org.mapstruct.Mapper;
+
+@Mapper(uses = {DateMapper.class})
+public interface BeerInventoryMapper {
+
+    BeerInventory beerInventoryDtoToBeerInventory(BeerInventoryDto beerInventoryDTO);
+
+    BeerInventoryDto beerInventoryToBeerInventoryDto(BeerInventory beerInventory);
+}
