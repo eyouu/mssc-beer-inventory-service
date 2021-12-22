8e23881c05cbf18bbf0ba4f9d95073213579c6e8
Vasyl <vazhovnir@gmail.com>
2021-12-22 19:18:53 +0200

    implement AllocationServiceImpl

---
+++ src/main/java/com/whosaidmeow/services/AllocationService.java
@@ -0,0 +1,8 @@
+package com.whosaidmeow.services;
+
+import com.whosaidmeow.brewery.model.BeerOrderDTO;
+
+public interface AllocationService {
+
+    Boolean allocateOrder(BeerOrderDTO beerOrderDTO);
+}
